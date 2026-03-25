46a480b1c492d11a1322c699d3c16c957dbe920d
Tanmaysinh <tanmaysinhkhant@gmail.com>
2026-03-25 10:44:02 +0530

    Change price and add palak tofu bowl

--- catering.js
+++ catering.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       "name": "Chicken Tikka Bowl",
       "variation": "Regular",
-      "price_cents": 1599,
+      "price_cents": 1699,
       "currency": "USD",
       "image_url": "https://132496695.cdn6.editmysite.com/uploads/1/3/2/4/132496695/GJZVJ3B2SXCGZ32D572L5JNR.png?width=2400&optimize=medium",
       "ecom_url": "https://www.amboeats.com/product/chicken-tikka-bowl/VF6ZWNNSKDZ6WIMBPZI7KKY2"
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       "name": "Chili Paneer Bowl",
       "variation": "Regular",
-      "price_cents": 1599,
+      "price_cents": 1699,
       "currency": "USD",
       "image_url": "https://132496695.cdn6.editmysite.com/uploads/1/3/2/4/132496695/BTKKASM7PXOFDDDJO7RUWW2V.png?width=2400&optimize=medium",
       "ecom_url": "https://www.amboeats.com/product/chilli-paneer-bowl/2WUYLCKCAIL4DJLMAT7YDKUD"
@@ -39,15 +39,23 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       "name": "Chili Shrimp Bowl",
       "variation": "Regular",
-      "price_cents": 1799,
+      "price_cents": 1850,
       "currency": "USD",
       "image_url": "https://132496695.cdn6.editmysite.com/uploads/1/3/2/4/132496695/MAIJLYFHZUZ6TEYZPTTUJOWM.png?width=2400&optimize=medium",
       "ecom_url": "https://www.amboeats.com/product/chilli-shrimp-bowl/4GUXZ4NX2MMNY4AZHORDTNBE"
     },
     {
+      "name": "Palak Tofu Bowl",
+      "variation": "Regular",
+      "price_cents": 1699,
+      "currency": "USD",
+      "image_url": "https://132496695.cdn6.editmysite.com/uploads/1/3/2/4/132496695/XUBYUKKQKSTMADNOXOO5ECEU.jpeg?width=2400&optimize=medium",
+      "ecom_url": "https://www.amboeats.com/product/palak-tofu-bowl/MCLJSKQTIYJCTAFJJSAHWHMV"
+    },
+    {
       "name": "Chili Shrimp with Okra Plate",
       "variation": "Regular",
-      "price_cents": 1700,
+      "price_cents": 1799,
       "currency": "USD",
       "image_url": "https://132496695.cdn6.editmysite.com/uploads/1/3/2/4/132496695/SBSSSHXRAQJVPC2EFLFKHZHD.png?width=2400&optimize=medium",
       "ecom_url": "https://www.amboeats.com/product/chilli-shrimp-with-okra-plate/AT4OG2K7C5VUSFZWNT2RRL7A"
@@ -55,7 +63,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       "name": "Veggie Plate",
       "variation": "Regular",
-      "price_cents": 1599,
+      "price_cents": 1699,
       "currency": "USD",
       "image_url": "https://132496695.cdn6.editmysite.com/uploads/1/3/2/4/132496695/TKKB2BBRRGID6IFSAMDX2ZY5.png?width=2400&optimize=medium",
       "ecom_url": "https://www.amboeats.com/product/veggie-plate/K2PWM7FOGF2W4M2BOZ3XNBRD"
@@ -63,7 +71,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       "name": "Paneer Tikka Masala with Chickpeas Plate",
       "variation": "Regular",
-      "price_cents": 1499,
+      "price_cents": 1650,
       "currency": "USD",
       "image_url": "https://132496695.cdn6.editmysite.com/uploads/1/3/2/4/132496695/LYV722LS6AVESUPSHS4PH6CY.png?width=2400&optimize=medium",
       "ecom_url": "https://www.amboeats.com/product/paneer-tikka-masala-with-chickpeas-plate/J4PZB63L7JO4ADB7ZD7L25QM"
@@ -71,7 +79,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       "name": "Chicken Tikka Masala with Okra Plate",
       "variation": "Regular",
-      "price_cents": 1499,
+      "price_cents": 1699,
       "currency": "USD",
       "image_url": "https://132496695.cdn6.editmysite.com/uploads/1/3/2/4/132496695/X4GM7BNZ4Z3VAZWSUHIL475L.png?width=2400&optimize=medium",
       "ecom_url": "https://www.amboeats.com/product/chicken-tikka-masala-with-okra-plate/PLACEHOLDER"
@@ -79,7 +87,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       "name": "Chicken Tikka Masala with Cauliflower Plate",
       "variation": "Regular",
-      "price_cents": 1499,
+      "price_cents": 1699,
       "currency": "USD",
       "image_url": "https://132496695.cdn6.editmysite.com/uploads/1/3/2/4/132496695/EGFRRVAHDTZZG7JMMZDZDGUV.png?width=2400&optimize=medium",
       "ecom_url": "https://www.amboeats.com/product/chicken-tikka-masala-with-cauliflower-plate/PLACEHOLDER"
@@ -87,7 +95,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       "name": "Low Carb Chicken Tikka with Cauliflower Plate",
       "variation": "Regular",
-      "price_cents": 1499,
+      "price_cents": 1699,
       "currency": "USD",
       "image_url": "https://132496695.cdn6.editmysite.com/uploads/1/3/2/4/132496695/ITUVIVSN2HXJMJLSBFLAN3WB.png?width=2400&optimize=medium",
       "ecom_url": "https://www.amboeats.com/product/low-carb-chicken-tikka-with-cauliflower-plate/PLACEHOLDER"
@@ -95,7 +103,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       "name": "Chicken Tikka Masala with Chickpeas Plate",
       "variation": "Regular",
-      "price_cents": 1499,
+      "price_cents": 1650,
       "currency": "USD",
       "image_url": "https://132496695.cdn6.editmysite.com/uploads/1/3/2/4/132496695/IEK47UDNBPER24NS3FM7PB2S.png?width=2400&optimize=medium",
       "ecom_url": "https://www.amboeats.com/product/chicken-tikka-masala-with-chickpeas-plate/PLACEHOLDER"
